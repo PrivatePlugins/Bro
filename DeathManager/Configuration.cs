15fd9ed30ffac5ccac67dc4f746a5e88596046f0
RoyIL <a4834833@gmail.com>
2016-11-11 20:53:30 +0200

    Update Configuration.cs

--- DeathManager/Configuration.cs
+++ DeathManager/Configuration.cs
@@ -1,21 +1,18 @@
 using Rocket.API;
 
-namespace DeathManager
+namespace DM
 {
     public class Configuration : IRocketPluginConfiguration
     {
         public bool Enabled = true;
 
-        public bool ShowErrorMessages ;
         public bool ShowSuicideMessages ;
         public bool ShowHeadshotMessages ;
-
         public string DeathMessagesColor;
 
         public void LoadDefaults()
         {
             Enabled = true;
-            ShowErrorMessages = true;
             ShowSuicideMessages = true;
             ShowHeadshotMessages = true;
             DeathMessagesColor = "Red";
